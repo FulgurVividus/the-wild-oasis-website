96efb5b873e463a2e1bb418a515bc351276f320e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-12 00:12:38 +0500

    chore: fetching Data in a Page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,18 @@
-function Page() {
+async function Page() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await res.json();
+
   return (
     <>
-      <h1>Cabins page</h1>
+      <div>
+        <h1>Cabins page</h1>
+
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
