0f60672bbd0665f3602e1dc8844a127cee4d9766
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-13 23:33:57 +0500

    chore: project organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,21 +1,8 @@
-import Counter from "../components/Counter";
-
-async function Page() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const data = await res.json();
-
+function Page() {
   return (
     <>
       <div>
         <h1>Cabins page</h1>
-
-        <ul>
-          {data.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-
-        <Counter users={data} />
       </div>
     </>
   );
