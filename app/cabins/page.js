69a9251dca719e08d60ebd88e83c0da3e4627218
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-24 19:41:33 +0500

    chore: experimenting with Caching and ISR

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,9 @@ import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 
+// time must be in seconds
+export const revalidate = 3600;
+
 export const metadata = {
   title: "Cabins",
 };
