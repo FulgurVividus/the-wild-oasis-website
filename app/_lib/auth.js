a35055f8a1cae65745af216ae2ab3d2ecc379f4a
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-30 19:34:41 +0500

    feat: creating a New Guest on First Sign-In

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 // config object
 const authConfig = {
@@ -12,6 +13,25 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingUser = await getGuest(user.email);
+
+        if (!existingUser) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      // we can mutate the session object. Onto the "user" we add the "guestId"
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
@@ -27,5 +47,11 @@ export const {
 } = NextAuth(authConfig);
 
 // auth - will be called in server components (example: to retrieve current session)
+
 // handlers - we use for logging in and signing out
-// signIn / signOut - will be called when signIn/signOut buttons are clicked.
+
+// signIn / signOut from NextAuth - will be called when signIn/signOut buttons are clicked.
+
+// signIn from callbacks - this actually runs before the signup process happens, that means that we can perform all kinds of operations that are associated with singing in process.
+
+// session from callbacks - this runs after the signIn callback and each time when the session is checked out.
