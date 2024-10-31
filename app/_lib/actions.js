c1bcfaae27f277770f726ea25def02f25bb6011e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-10-31 22:00:59 +0500

    feat: Manual Cache Revalidation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -40,4 +41,6 @@ export async function updateGuestAction(formData) {
   if (error) {
     throw new Error("Guest could not be updated");
   }
+
+  revalidatePath("/account/profile");
 }
