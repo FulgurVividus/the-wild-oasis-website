52ae53ba95d6293b9c7048aed1e18fce040e8cb6
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-11-03 21:19:33 +0500

    feat: Removing Reservations Immediately (the useOptimistic hook)

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,36 @@
+"use client";
+
+import ReservationCard from "./ReservationCard";
+import { useOptimistic } from "react";
+import { deleteReservationAction } from "@/app/_lib/actions";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (currentBookings, bookingId) => {
+      // return next optimistic state
+      return currentBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    // optimistically deleting in the UI
+    optimisticDelete(bookingId);
+    // optimistically deleting on the server
+    await deleteReservationAction(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          onDelete={handleDelete}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
